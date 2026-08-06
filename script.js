da3a144bdd22c1808f3a33f1be817dd93f90d96e
Louis Stiller <louis.stiller.05@gmail.com>
2026-08-06 18:59:03 +0200

    Add update and delete functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://task-api-u38p.onrender.com/tasks?size=50";
+const API_URL = "https://task-api-u38p.onrender.com/tasks";
 const taskList = document.getElementById("taskList");
 const addButton = document.getElementById("addButton")
 const taskTitle= document.getElementById("taskTitle");
@@ -11,25 +11,52 @@ body: JSON.stringify({title: title})});
 console.log(response.status);
 if(response.ok){
 taskTitle.value="";
-await loadTasks()
+await loadTasks();
 }
 }
 addButton.addEventListener("click", addTask);
 
+async function deleteTask(id){
+    const response= await fetch(`${API_URL}/${id}`,{
+        method:"DELETE"});
+console.log(response.status);
+
+await loadTasks();
+}
+
+async function updateTask(id, title, completed){
+const response= await fetch(`${API_URL}/${id}`,{
+    method:"PUT",
+    headers:{"Content-Type":"application/json"},
+    body: JSON.stringify({title:title, completed:completed})
+});
+console.log(response.status);
+if(response.ok){
+await loadTasks();
+}
+}
 
 async function loadTasks() {
 
-    const response = await fetch(API_URL, {
-    cache: "no-store"
-});
+    const response = await fetch(`${API_URL}?size=50`);
 
     const page = await response.json();
 
-    taskList.innerHTML="";
+    taskList.innerHTML = "";
 
     for(const task of page.content){
         const listItem = document.createElement("li");
-        listItem.textContent= task.title;
+        listItem.textContent= task.title +", "+ task.completed;
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Löschen";
+        deleteButton.addEventListener("click", () => deleteTask(task.id));
+        const completeButton= document.createElement("button");
+        completeButton.textContent= "Erledigt";
+        completeButton.addEventListener("click", function(){
+            updateTask(task.id, task.title, !task.completed);
+        })
+        listItem.appendChild(deleteButton);
+        listItem.appendChild(completeButton);
         taskList.appendChild(listItem);
     }
 
